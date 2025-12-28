b6f7baa46069c22e0c11c691148cb39292a31b7b
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-29 01:07:44 +0530

    Create department_with_highest_avg_salary.sql

---
+++ 01_Data_Analytics/SQL/08_Interview_Questions/department_with_highest_avg_salary.sql
@@ -0,0 +1,12 @@
+/*
+Interview Question: Department with highest average salary
+Concepts: GROUP BY, ORDER BY
+*/
+
+SELECT
+    department,
+    AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department
+ORDER BY avg_salary DESC
+LIMIT 1;
